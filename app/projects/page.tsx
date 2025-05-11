8165b5d4160bd59d6030fbf9e001dfefdb0beb76
chaim <chaim.triebitz@bhbsdventures.com>
2025-05-11 18:52:44 +0300

    feat: Add new project "REAL ESTATE LANDING PAGE 2" and update technologies for existing projects
    - Introduced a new project entry with details for a real estate landing page, including technologies used and links to GitHub and live site.
    - Updated the technologies array for the "CRUD TABLE APP" project to include "Reducer" and "context".
    - Adjusted project IDs for consistency following the addition of the new project.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,7 +9,7 @@ const ProjectsPage = () => {
     {
       title: "CRUD TABLE APP",
       description: "Authenticated crud table app with connected to a database",
-      technologies: ["React", "Node.js", "MongoDB"],
+      technologies: ["React", "Node.js", "MongoDB", "Reducer","context"],
       imageUrl: "/projects/proj.crud-table.png",
       githubUrl: "https://github.com/ChaimTriebitz/crud-table-front",
       githubUrl2: "https://github.com/ChaimTriebitz/crud-table-back",
@@ -27,13 +27,23 @@ const ProjectsPage = () => {
       id: 2,
     },
     {
+      title: "REAL ESTATE LANDING PAGE 2",
+      description:
+        "Landing page for a real estate company with a modern design and animations",
+      technologies: ["Next.js", "Tailwind CSS", "TypeScript","Framer Motion"],
+      imageUrl: "/projects/proj.shaarei-chesed.png",
+      githubUrl: "https://github.com/ChaimTriebitz/shaarei-chesed-frontend",
+      liveUrl: "https://shaarei-chesed-frontend.vercel.app/",
+      id: 3,
+    },
+    {
       title: "LEWYER WEBSITE",
       description: "Website for a lewyer with some cool animations",
       technologies: ["animation", "SCSS", "responsive"],
       imageUrl: "/projects/proj.lewyer.png",
       githubUrl: "https://github.com/ChaimTriebitz/freiman-frontend",
       liveUrl: "https://www.fraiman-law-firm.com/",
-      id: 3,
+      id: 4,
     },
     {
       title: "TRIP EXPERIENCE SHARING APP",
@@ -43,7 +53,7 @@ const ProjectsPage = () => {
       imageUrl: "/projects/proj.trip.png",
       githubUrl: "https://github.com/ChaimTriebitz/trip-frontend/",
       liveUrl: "https://trip-front-end.onrender.com/",
-      id: 4,
+      id: 5,
     },
   ];
 
